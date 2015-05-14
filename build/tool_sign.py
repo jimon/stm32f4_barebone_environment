d1d75b3bd876f39154ca31ab82dc153998d06d72
Dmitry Ivanov <jimon.j1m0n@gmail.com>
2015-05-14 12:39:51 +0200

    python tools

--- build/tool_sign.py
+++ build/tool_sign.py
@@ -1,19 +1,18 @@
-#!/usr/bin/python3
+# stm32 binary sign
 import hashlib
 import struct
 import sys
-if len(sys.argv) < 4 :
-	print("wrong invocation")
+if len(sys.argv) < 4:
+	print("wrong invocation, usage : python toolsign.py $in $out $FLASH_SIGNOFFSET")
 	exit(1)
 prefixlen = int(sys.argv[3], 0)
 
-with open(sys.argv[1], "rb") as infile :
-	fw=infile.read()
-	with open(sys.argv[2], "wb") as outfile :
+with open(sys.argv[1], "rb") as infile:
+	fw = infile.read()
+	with open(sys.argv[2], "wb") as outfile:
 		if prefixlen > 0 :
-			ha=hashlib.md5(fw).digest()
-			le=len(fw)
-			prefix=struct.pack("<I16s", le, ha).ljust(prefixlen, b"\0")
+			ha = hashlib.md5(fw).digest()
+			le = len(fw)
+			prefix = struct.pack("<I16s", le, ha).ljust(prefixlen, b"\0")
 			outfile.write(prefix)
 		outfile.write(fw)
-
---
+++ build/tool_strrepl.py
@@ -0,0 +1,13 @@
+# string replace in files
+import sys
+if len(sys.argv) < 3:
+	print("wrong invocation, usage : python tool_strrepl.py $in $out $replace_from_1 $replace_to_1 $replace_from_2 $replace_to_2 ...")
+	exit(1)
+
+with open(sys.argv[1], "rb") as infile:
+	fw = infile.read()
+	with open(sys.argv[2], "wb") as outfile:
+		for i in range(0, (len(sys.argv) - 3) / 2):
+			fw = fw.replace(sys.argv[i * 2 + 3], sys.argv[i * 2 + 4])
+		outfile.write(fw)
+
